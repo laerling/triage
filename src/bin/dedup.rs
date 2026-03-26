aabd5c2af200db35b0fd18ccfcec5de480a5c345
laerling <laerling@posteo.de>
2026-03-27 00:03:38 +0100

    dedup: Improve error handling

--- src/bin/dedup.rs
+++ src/bin/dedup.rs
@@ -1,6 +1,6 @@
 use std::env::args;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Result};
 use std::os::unix::fs::MetadataExt;
 use std::path::Path;
 use std::process::exit;
@@ -9,15 +9,14 @@ use xxhash_rust::xxh64::Xxh64;
 const CHUNK_SIZE: usize = 1024 * 1024 * 10;
 
 // print hash and inode number of file
-fn file(f: &Path) {
+fn file(f: &Path) -> Result<()> {
     if !f.is_file() {
         eprint!("Not a file: {}", f.display());
         exit(1);
     }
 
     // get inode of file
-    let md = f.metadata().unwrap_or_else(
-        |_| panic!("Can't read metadata from file: {:?}", f));
+    let md = f.metadata()?;
     let inode: u64 = md.ino();
 
     // get hash of file
@@ -25,8 +24,7 @@ fn file(f: &Path) {
     // stream file content into hash function instead of loading the complete file into memory (I
     // have some multiple GBs large files)
     let flen: u64 = md.len();
-    let mut fh: File = File::open(f).unwrap_or_else(
-        |_| panic!("Cannot open file: {:?}", f));
+    let mut fh: File = File::open(f)?;
 
     // we have to use a vector to make sure we completely stay on the heap, lest we overflow the
     // stack. Even Box::new([0;CHUNK_SIZE]) would overflow the stack...
@@ -37,23 +35,23 @@ fn file(f: &Path) {
     let mut hasher = Xxh64::new(0);
     let mut remaining_bytes: u64 = flen;
     while remaining_bytes > CHUNK_SIZE as u64 {
-        fh.read_exact(&mut chunk_buf).unwrap_or_else(
-            |_| panic!("Cannot read {} bytes from file: {:?}", CHUNK_SIZE, f));
+        fh.read_exact(&mut chunk_buf)?;
         hasher.update(chunk_buf.as_slice());
         remaining_bytes -= CHUNK_SIZE as u64;
     }
 
     // read last few bytes
     let mut rest: Vec<u8> = Vec::new();
-    fh.read_to_end(&mut rest).unwrap_or_else(
-        |_| panic!("Failed reading rest of file: {:?}", f));
+    fh.read_to_end(&mut rest)?;
     hasher.update(rest.as_slice());
 
     // finalize hasher
     let hash: u64 = hasher.digest();
 
-    // print hash, inode and file name
-    println!("{:016x} {} {}", hash, inode, f.display());
+    // print hash, inode, file size and file name
+    println!("{:016x} {} ({} bytes) {}", hash, inode, flen, f.display());
+
+    Ok(())
 }
 
 // recurse into directory
@@ -64,8 +62,8 @@ fn dir(d: &Path) {
     }
 
     // recurse
-    for entry in d.read_dir().unwrap_or_else(
-        |_| panic!("Cannot read directory: {:?}", d))
+    for entry in d.read_dir()
+        .unwrap_or_else(|_| panic!("Can't read directory: {:?}", d))
     {
         let entry = entry.unwrap_or_else(
             |_| panic!("Can't read entry from directory {:?}", d));
@@ -84,7 +82,10 @@ fn dispatch(p: &Path) {
     if p.is_dir() {
         dir(&p);
     } else if p.is_file() {
-        file(&p);
+        match file(&p) {
+            Ok(()) => {},
+            Err(e) => eprintln!("Error in file {p:?}: {e}"),
+        };
     } else {
         eprint!("Ignoring item, since it's none of file, directory, symlink: {}\n", p.display());
     }
